6932e773aec5bbadebd5e0f732b869a56f78117e
1eo1 <60741464+1eo1@users.noreply.github.com>
2023-03-07 17:31:05 +0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -15,7 +15,7 @@ read -p "Repeat password: " passtwo;
 if [ $passone = $passtwo ];
 then
 	touch /data/data/com.termux/files/.pass
-	python -c "import hashlib; print(hashlib.sha1(b'$passone').hexdigest())" > /data/data/com.termux/files/.pass
+	python -c "import hashlib; print(hashlib.sha256(b'$passone').hexdigest())" > /data/data/com.termux/files/.pass
 	echo 'Login installed'
 
 elif [ $passone != $passtwo ];
