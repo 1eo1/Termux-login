ccfd8fce5eccb997f3387ebac687cdaca21f6ad9
1eo1 <60741464+1eo1@users.noreply.github.com>
2023-03-07 17:07:11 +0500

    Update login.py

--- login.py
+++ login.py
@@ -1,4 +1,4 @@
-#!/data/data/com.termux/files/usr/bin/env python
+    #!$PREFIX/bin/env python
 
 import getpass
 import hashlib
@@ -7,14 +7,14 @@ import os
 
 password = getpass.getpass(prompt='| ')
 
-filepass = open("/data/data/com.termux/files/usr/share/login/.pass", "r")
+filepass = open("/data/data/com.termux/files/.pass", "r")
 filepass = filepass.read().split("\n")[0]
 
 password = password.encode()
-password = hashlib.sha1(password).hexdigest()
+password = hashlib.sha256(password).hexdigest()
 
 if password != filepass:
-    print("Invalid password")
+    print("")
     os.system("exit")
 else:
     prefix = "/data/data/com.termux/files/usr"
