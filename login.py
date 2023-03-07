3151107ffc0a554b173ff2932ccbbb858d866505
1eo1 <60741464+1eo1@users.noreply.github.com>
2023-03-07 16:37:09 +0500

    change prompt text

--- login.py
+++ login.py
@@ -5,7 +5,7 @@ import hashlib
 import sys
 import os
 
-password = getpass.getpass(prompt='| ', stream=None)
+password = getpass.getpass(prompt='| ')
 
 filepass = open("/data/data/com.termux/files/usr/share/login/.pass", "r")
 filepass = filepass.read().split("\n")[0]
